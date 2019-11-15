8894f1ec54ce3e02a458305f4e363c9bd78e07e6
Colby Emann <colbyemann2010@gmail.com>
2019-11-14 18:53:59 -0700

    Created search layout

--- browse-search.php
+++ browse-search.php
@@ -1,11 +1,17 @@
-<?php include("includes/header.inc.php")?>
+<?php 
+    include("includes/header.inc.php");
+    include("includes/search.inc.php");
+?>
 <!DOCTYPE html>
 <html>
 <?php buildHead(); ?>
-<title>Browse/Search</title>  
+<title>Browse/Search</title> 
+<link rel="stylesheet" href="css/searchlayout.css">   
 </head>
 <body>
 <?php buildMenu(); ?>
-
+<main class="container">
+    <?php buildSearch(); ?>
+</main>
 </body>
 </html>
