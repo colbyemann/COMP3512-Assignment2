f2a8a1b54927b8321a8f89427b91ae3560a970da
Colby Emann <colbyemann2010@gmail.com>
2019-11-22 15:06:34 -0700

    Live DB API

---
+++ includes/configlive.ini.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DBNAME', 'pv6ckb3kah9ykgpa');
+define('DBUSER', 'tkvd4h5t0qak1d2p');
+define('DBPASS', 'ik8znm2000si7662');
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+?>
