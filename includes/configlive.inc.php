8e49ec73ef828cad2cbf7b944a129b00dc6793c3
bstuike <bstuike@gmail.com>
2019-11-22 17:08:12 -0700

    Change file extension from ini.php to inc.php

--- includes/configlive.inc.php
+++ includes/configlive.inc.php
@@ -4,6 +4,4 @@ define('DBNAME', 'pv6ckb3kah9ykgpa');
 define('DBUSER', 'tkvd4h5t0qak1d2p');
 define('DBPASS', 'ik8znm2000si7662');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-
-
 ?>
