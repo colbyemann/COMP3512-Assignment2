209b249c4ddab865452ad85f3539ab9f29106d8f
bstuike <bstuike@gmail.com>
2019-11-24 13:02:24 -0700

    Indentation/spacing check

--- api-countries.php
+++ api-countries.php
@@ -2,55 +2,44 @@
 require_once('includes/config.inc.php');
 require_once('includes/db-functions.inc.php');
 
-
 $connection = setConnectionInfo(DBCONNSTRING,DBUSER,DBPASS);
 
-
-
-if(isset($_GET['ISO']))
-    {
-      $result = getCountriesByISO($connection, $_GET['ISO']);
-      echo $result;
-    }
-else{
+if(isset($_GET['ISO'])) {
+   $result = getCountriesByISO($connection, $_GET['ISO']);
+   echo $result;
+}
+else {
    $result = getAllCountries($connection);
    echo $result;
-   };
-
-
-
+};
 
 function getCountriesSQL() {
-    //change this to countires
-    $sql = "SELECT ISO, CountryName, Capital, CityCode, Area, Population, Continent, TopLevelDomain, CurrencyCode, Languages, Neighbours, CountryDescription FROM countries";
-    return $sql;
- }
+   //change this to countires
+   $sql = "SELECT ISO, CountryName, Capital, CityCode, Area, Population, Continent, TopLevelDomain, CurrencyCode, Languages, Neighbours, CountryDescription FROM countries";
+   return $sql;
+}
 
- function getAllCountries($connection) {
-    $sql = getCountriesSQL();
-    $rows = array();
-    try {
+function getAllCountries($connection) {
+   $sql = getCountriesSQL();
+   $rows = array();
+   try {
       $result = runQuery($connection, $sql, null);
-      
 
       while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      $rows[] = $row;
-      
+         $rows[] = $row;
       }
       return json_encode($rows);
-      
    }
    catch (PDOException $e) {
       die( $e->getMessage() );
    }   
-  }
+}
 
-  function getCountriesByISO($connection, $iso) {
+function getCountriesByISO($connection, $iso) {
+   $sql = getCountriesSQL();
+   $sql = $sql . " WHERE ISO='$iso'";
     
-    $sql = getCountriesSQL();
-    $sql = $sql . " WHERE ISO='$iso'";
-    
-    $result = runQuery($connection, $sql, null);
+   $result = runQuery($connection, $sql, null);
    // Covert to JSON
 
    $rows = array();
@@ -58,7 +47,7 @@ function getCountriesSQL() {
    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
       $rows[] = $row;
       
-      }
-      return json_encode($rows);
    }
+   return json_encode($rows);
+}
 ?>
