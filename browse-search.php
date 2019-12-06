2f81211a62bf2fe9340027f0a747f3fe4fab6393
jwola586 <jwola586@mtroyal.ca>
2019-12-05 18:35:22 -0700

    Search by cities updated and view button linked

--- browse-search.php
+++ browse-search.php
@@ -38,20 +38,36 @@
                 ?>
                     
                 </form>
+                
             <?php
             
             if($_SERVER["REQUEST_METHOD"] == "GET") {
                if (isset($_GET['ISO'])) {
                    outputCountryPhotos($_GET['ISO']);
                    
+            ?>
+                <form method="get" action="<?=$_SERVER['REQUEST_URI']?>">
+                <button class="button" type="submit"> Filter </button>   
+            <?php
+                   populateCityList($_GET['ISO']);
+            ?>
+                </form>
+                    
+            <?php     
                    
                } 
-               else
+               else if (isset($_GET['CityCode']))
                {
-                   outputPhotos();
+                   outputCityPhotos($_GET['CityCode']);
                }
+                else
+                {
+                    outputPhotos();
+                }
             }
-    
+            
+            
+
             
                
                 
@@ -80,6 +96,13 @@ function getPhotosByCountry($iso)
     return $photos;
 }
 
+function getPhotosByCity($code)
+{
+    $data = file_get_contents("http://localhost/Assignment_2/api-photos.php?CityCode=" . $code);
+    $photos = json_decode($data, true);
+    return $photos;
+}
+
 
 function getCountries()
 {
@@ -88,6 +111,15 @@ function getCountries()
     return $countries;
 
 } 
+
+function getCitiesByISO($iso)
+{
+    $data = file_get_contents("http://localhost/Assignment_2/api-cities.php?ISO=" . $iso);
+    $cities = json_decode($data, true);
+    return $cities;
+
+} 
+
 
 function populateCountryList()
 {
@@ -111,9 +143,37 @@ function populateCountryList()
                 $newCountry = false;
                 
             }
-            
-            
            
+        }
+
+    }
+    echo "</select>";
+}
+
+function populateCityList($iso)
+{
+    $cities = getCitiesByISO($iso);
+    $photos = getPhotos();
+
+    
+    echo "<select name=CityCode>";
+    echo "<option value='DEF'>Select a City</option>";
+    
+    foreach($cities as $c)
+    {
+        $newCity = true;
+        
+        foreach($photos as $p)
+        {
+            
+            if($p['CityCode'] == $c['CityCode'] && $newCity == true)
+            {
+                
+                echo "<option value='".$c['CityCode']."'>". $c['AsciiName'] . "</option>";
+                $newCity = false;
+                
+            }
+            
         }
 
     }
@@ -123,6 +183,14 @@ function populateCountryList()
 function outputCountryPhotos($iso)
 {
     $photos = getPhotosByCountry($iso);
+    foreach($photos as $p) {
+        outputSinglePhoto($p);
+    }
+}
+
+function outputCityPhotos($code)
+{
+    $photos = getPhotosByCity($code);
     foreach($photos as $p) {
         outputSinglePhoto($p);
     }
@@ -141,7 +209,11 @@ function outputPhotos()
 function outputSinglePhoto($photo)
 {
    echo "<img src='images/square150/". $photo['Path'] . "'>";
-   echo "<button type=button>View</button>";
+   //echo "<p>" . $photo['Title'] . "</p>";
+    
+    echo "<a href='http://localhost/Assignment_2/single-photo.php?ImageID=". $photo['ImageID']  ."'><button type=button action=>View</button></img></a>";
+
+   
    echo "<button type=button>Add to Favourites</button>";
 
 }
