764637ee75dfe9ec3360ebc0c0e0679f3b25b596
jwola586 <jwola586@mtroyal.ca>
2019-12-05 18:36:33 -0700

    Changed caps

--- api-photos.php
+++ api-photos.php
@@ -8,9 +8,9 @@ if(isset($_GET['ISO'])) {
       $result = getPhotosByISO($connection, $_GET['ISO']);
       echo $result;
 }
-else if(isset($_GET['citycode']))
+else if(isset($_GET['CityCode']))
 {
-   $result = getPhotosByCode($connection, $_GET['citycode']);
+   $result = getPhotosByCode($connection, $_GET['CityCode']);
       echo $result;
 }
 else {
