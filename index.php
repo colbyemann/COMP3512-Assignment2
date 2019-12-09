9a1b50928bee791c88bd26e9ebc9ba8e88e2d9a2
Colby Emann <colbyemann2010@gmail.com>
2019-12-08 21:48:14 -0700

    Remove Files

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include("includes/home.inc.php");
                 buildPopSignup();
             ?>
         </main>
-        <script src="js/storage.js"></script>
+        
         <script src="js/pop.js"></script>
     </body>
 </html>
