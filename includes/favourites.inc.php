6f264980696ed375e47d0ca7dc0f1d6db3f24b2f
Byron Stuike <bstuike@gmail.com>
2019-12-08 18:03:08 -0700

    Remove partially working

--- includes/favourites.inc.php
+++ includes/favourites.inc.php
@@ -3,16 +3,23 @@
 function buildFavouritesPage()
 {
     $remove = true;
+    $removeAll = true;
     echo "
         <div class='box favs'>
             <h3>Your Favourited Images</h3>
-                <div class='buffer'><input id='favs' type='button' value='Remove All'></div>";
-                foreach($_SESSION['favPhoto'] as $f) {
-                    echo "<div class='buffer'><a href='" . "?Path=" . $f['Path'] . "&amp;ImageID=" . $f['ImageID'] . "&amp;CountryCodeISO=" . 
-                    $f['CountryCodeISO'] . "&amp;CityCode=" . $f['CityCode'] . "&amp;Remove=" . $remove . "'><img src='images/square150/" . $f['Path'] . "'>
-                    <input id='favs' type='button' value='Remove from Favourites'></div></a>";
+                <div class='buffer'><a href='" . "?RemoveAll=" . $removeAll . "'><input id='favs' type='button' value='Remove All'></div>";
+                //if(isset($_GET['RemoveAll'])) {$_SESSION['favPhoto'] = null;}
+
+                if($_SESSION['favPhoto'] == null) {}
+                else {
+                    foreach($_SESSION['favPhoto'] as $f) {
+                        echo "<div class='buffer'><a href='" . "?Path=" . $f['Path'] . "&amp;ImageID=" . $f['ImageID'] . "&amp;CountryCodeISO=" . 
+                        $f['CountryCodeISO'] . "&amp;CityCode=" . $f['CityCode'] . "&amp;Remove=" . $remove . "'><img src='images/square150/" . $f['Path'] . "'>
+                        <input id='favs' type='button' value='Remove from Favourites'></div></a>";
+                    }
+                    if(isset($_GET['Remove'])) {removeSessionElement($f['ImageID'], $f['Path'], $f['CountryCodeISO'], $f['CityCode']);}
                 }
-                if(isset($_GET['Remove'])) {removeSessionElement($f['ImageID'], $f['Path'], $f['CountryCodeISO'], $f['CityCode']);}
+                
         echo "
         </div>";
 }
