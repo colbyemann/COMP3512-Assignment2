80dc5a06ae7846ade8a94e73be99da9443eafcb6
Byron Stuike <bstuike@gmail.com>
2019-12-10 16:44:51 -0700

    Home page overhaul

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -2,19 +2,17 @@
 
 function buildHomePage()
 {
-	echo "<div class='box'>
-		<div class='heroimage'>	
-			<!-- Image retrieved from https://images.unsplash.com/photo-1573554394544-930a4cf6fc66?ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80 -->
-			<img src='images/vista.jpg'>
-		</div><br>
+	echo "<div class='indexbox'>
+    <h1>Welcome to World Explorer Online</h1>
 		<div class='loginbuttons'>
 				<input id='login' type='button' onclick='openLoginForm()' value='LOGIN'>
 				<input id='join' type='button' onclick='openSignupForm()' value='JOIN'>
 		</div><br>
-		<div class='searchbar'>
+		<div class='searcharea'>
+        <h2>Search for a photo of your choice</h2>
         <form method='get' action='browse-search.php?'>
-				<input type='text' id='text' name='Title' placeholder='Search Box for Photos'>
-                <button class='button' type='submit'> Search Text </button>
+        <input type='text' id='text' name='Title' placeholder='Search Box for Photos'>
+        <button class='button' type='submit'> Search Text </button>
         </form>
 		</div>
 	</div>";
