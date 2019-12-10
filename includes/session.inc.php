3f53b31415e1f848707b56d5785d65d2becef4e6
Byron Stuike <bstuike@gmail.com>
2019-12-10 15:59:48 -0700

    Comments for session.inc file

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+// Tests the session state. If logged in navigation is allowed, if not, user is redirected to the index page
 session_start();
 if(!isset($_SESSION['logged_in'])) {
    header('Location: index.php');
