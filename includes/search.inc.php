4ea34de9722f09e30e83d2396d6b233b1e8266f9
Byron Stuike <bstuike@gmail.com>
2019-12-10 15:55:42 -0700

    Comments for search.inc file

--- includes/search.inc.php
+++ includes/search.inc.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+// Creates the select list for all countries
 function populateCountryList() {
     $countries = getAllCountries();
     $photos = getAllPhotos();
@@ -21,6 +20,7 @@ function populateCountryList() {
     echo "</select>";
 }
 
+// Creates the select list for all available cities based on country
 function populateCityList($iso) {
     $cities = getCitiesByISO($iso);
     $photos = getAllPhotos();
@@ -40,7 +40,7 @@ function populateCityList($iso) {
     echo "</select>";
 }
 
-
+// Searches for images by title, outputs message if not found
 function filterTitle($search) {
     $noImage = true;
     $photos = getAllPhotos();
@@ -65,16 +65,18 @@ function filterTitle($search) {
     }
 }
 
+// Searches the photo API by title and calls the outputSinglePhoto function
 function outputSearchPhotos($title) {
     $photos = getPhotosByTitle($title);
     
     if(is_array($photos)){
-    foreach($photos as $p) {
-        outputSinglePhoto($p);
-    }      
-}
+        foreach($photos as $p) {
+            outputSinglePhoto($p);
+        }
+    }
 }
 
+// Searches the photo API by ISO and calls the outputSinglePhoto function
 function outputCountryPhotos($iso) {
     $photos = getPhotosByISO($iso);
     foreach($photos as $p) {
@@ -82,6 +84,7 @@ function outputCountryPhotos($iso) {
     }
 }
 
+// Searches the photo API by City Code and calls the outputSinglePhoto function
 function outputCityPhotos($code) {
     $photos = getPhotosByCityCode($code);
     foreach($photos as $p) {
@@ -89,6 +92,7 @@ function outputCityPhotos($code) {
     }
 }
 
+// Searches the photo API and calls the outputSinglePhoto function to output all photos
 function outputPhotos() {
     $photos = getAllPhotos();
     foreach($photos as $p) {
@@ -96,6 +100,7 @@ function outputPhotos() {
     }
 }
 
+// Displays the image, title, and appropriate buttons based on login status
 function outputSinglePhoto($photo) {
     echo "<div id='divider'><figure><img src='images/square150/". $photo['Path'] . "'>
     <figcaption>" . $photo['Title'] . "</figcaption></figure>
@@ -104,6 +109,7 @@ function outputSinglePhoto($photo) {
     echo "</div>";
 }
 
+// Tests login status and displays add to favourites button if not already added
 function showButton($photo) {
     $add = true;
     $filter = true;
@@ -116,12 +122,14 @@ function showButton($photo) {
     }
 }
 
+// Adds a favourite to the seesion array and loads a pop-up alert message
 function addSessionElement() {
     $data = ['ImageID' => $_GET['ImageID'],'Path' => $_GET['Path'], 'CountryCodeISO' => $_GET['CountryCodeISO'], 'CityCode' => $_GET['CityCode']];
     array_push($_SESSION['favPhoto'], $data);
     echo "<script type='text/javascript'>alert('Photo added to Favourites');</script>";
 }
 
+// Removes a favourite from the seesion array and loads a pop-up alert message
 function removeSessionElement($id, $path, $iso, $citycode) {
     $data = ['ImageID' => $id, 'Path' => $path, 'CountryCodeISO' => $iso, 'CityCode' => $citycode];
     $index = array_search($data, $_SESSION['favPhoto']);
